83ee8db12503f23128e805a8a31d0e3555f95d63
ahmedhassan187 <ahmedhassann187@gmail.com>
2025-03-21 00:29:30 +0200

    edit printing statement

--- Try.py
+++ Try.py
@@ -5,8 +5,8 @@ BATCH_SIZE = 64
 
 
 # Generate dummy inputs with shape (10, 256, 256, 1)
-img_true = np.random.randint(0, 1, size=(10, 256, 256, 1), dtype=np.uint8)
-img_pred = np.random.randint(0, 1, size=(10, 256, 256, 1), dtype=np.uint8)
+img_true = np.random.randn(0, 1, size=(10, 256, 256, 1), dtype=np.uint8)
+img_pred = np.random.randn(0, 1, size=(10, 256, 256, 1), dtype=np.uint8)
 
 losses_class = losses(img_true,img_pred)
 print(f"Psnr for the Random images is = {losses_class.psnr()}")
