440ed1470e68442ee8d17060e4a8a4c51554335b
ahmedhassan187 <ahmedhassann187@gmail.com>
2025-03-20 23:41:28 +0200

    add Dataloader and try file to try make unit test for each module

--- Try.py
+++ Try.py
@@ -2,13 +2,13 @@ from dataloader import DataLoader
 
 BATCH_SIZE = 64
 
-# data_loader_Motion_Simulated = DataLoader(
-#     data_path="/kaggle/input/mmmai-simulated-data/ds004795-download",
-#     split_ratio=[0.7, 0.2, 0.1],
-#     view="Axial",
-#     data_id="Motion_Simulated",
-#     crop=False,
-#     batch_size=BATCH_SIZE,
-#     split_json_path=None
-# )
+data_loader_Motion_Simulated = DataLoader(
+    data_path="/kaggle/input/mmmai-simulated-data/ds004795-download",
+    split_ratio=[0.7, 0.2, 0.1],
+    view="Axial",
+    data_id="Motion_Simulated",
+    crop=False,
+    batch_size=BATCH_SIZE,
+    split_json_path=None
+)
 
