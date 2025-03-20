9a9016751b6a3f5c9e249faecd1fa1c00ac697af
ahmedhassan187 <ahmedhassann187@gmail.com>
2025-03-21 00:27:57 +0200

    edit printing statement

--- Try.py
+++ Try.py
@@ -10,9 +10,9 @@ img_pred = np.random.randint(0, 1, size=(10, 256, 256, 1), dtype=np.uint8)
 
 losses_class = losses(img_true,img_pred)
 print(f"Psnr for the Random images is = {losses_class.psnr()}")
-print(f"Psnr for the Random images is = {losses_class.ssim_loss()}")
-print(f"Psnr for the Random images is = {losses_class.ssim_score()}")
-print(f"Psnr for the Random images is = {losses_class.perceptual_loss()}")
+print(f"SSIM_Loss for the Random images is = {losses_class.ssim_loss()}")
+print(f"SSIM_socre for the Random images is = {losses_class.ssim_score()}")
+print(f"Per_loss for the Random images is = {losses_class.perceptual_loss()}")
 data_loader_Motion_Simulated = DataLoader(
     data_path="/kaggle/input/mmmai-simulated-data/ds004795-download",
     split_ratio=[0.7, 0.2, 0.1],
