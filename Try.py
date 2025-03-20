1b5c3f5147a3c703b9c50370840dcd95c2fd9654
ahmedhassan187 <ahmedhassann187@gmail.com>
2025-03-21 00:13:26 +0200

    add losses and metrics file and adding testing statements in the try.py file

--- Try.py
+++ Try.py
@@ -1,7 +1,17 @@
 from dataloader import DataLoader
-
+from losses_metrics import losses
+import numpy as np
 BATCH_SIZE = 64
 
+
+img_true = np.random.randint((10,256,256))
+img_pred = np.random.randint((10,256,256))
+
+losses_class = losses(img_true,img_pred)
+print(f"Psnr for the Random images is = {losses_class.psnr()}")
+print(f"Psnr for the Random images is = {losses_class.ssim_loss()}")
+print(f"Psnr for the Random images is = {losses_class.ssim_score()}")
+print(f"Psnr for the Random images is = {losses_class.perceptual_loss()}")
 data_loader_Motion_Simulated = DataLoader(
     data_path="/kaggle/input/mmmai-simulated-data/ds004795-download",
     split_ratio=[0.7, 0.2, 0.1],
